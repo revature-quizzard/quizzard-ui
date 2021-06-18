29073fac1cdef16412db18b34ae0ffaedf9a00d9
diuguide <everett.diuguid@gmail.com>
2021-06-17 20:42:56 -0400

    added react router dom, @types react router dom created skeleton component directory, added switch and routes to app.tsx, styled links a lil bit, added font script tag

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Main from "./components/Main";
+import Navigation from "./components/NavBar";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import FlashCard from "./components/Flashcards";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world!</h1>
-    </div>
+    <Router>
+      <Navigation />
+      <Container>
+        <Switch>
+          <Route exact path="/">
+            <Main />
+          </Route>
+          <Route exact path="/login">
+            <Login />
+          </Route>
+          <Route exact path="/register">
+            <Register />
+          </Route>
+          <Route exact path="/flashcards">
+            <FlashCard />
+          </Route>
+        </Switch>
+      </Container>
+    </Router>
   );
 }
 
