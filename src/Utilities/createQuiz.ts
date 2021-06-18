65e93534c4e179c3ae4b391ec6d6a95304ecd1da
diuguide <everett.diuguid@gmail.com>
2021-06-18 14:58:01 -0400

    completed random question generator function, imported into component

---
+++ src/Utilities/createQuiz.ts
@@ -0,0 +1,59 @@
+// Create dummy flashcards
+interface FlashCard {
+  id: number;
+  question: string;
+  answer: string;
+}
+
+export const createQuiz = (studySet: any) => {
+
+  studySet.forEach((flashCard: FlashCard) => {
+    
+    let newStudySet = studySet.filter((card: FlashCard) => {
+      if (card.question !== flashCard.question) {
+        return card;
+      }
+      return null;
+    });
+   
+    let wrongAnswerArray: string[] = [];
+
+    let ranNum: number = generateRandom(newStudySet.length);
+
+    while (wrongAnswerArray.length < 3) {
+      if (!wrongAnswerArray.includes(newStudySet[ranNum].answer)) {
+        wrongAnswerArray.push(newStudySet[ranNum].answer);
+      } else {
+        ranNum = generateRandom(newStudySet.length);
+      }
+    }
+
+    let wrong1: string = wrongAnswerArray[0];
+    let wrong2: string = wrongAnswerArray[1];
+    let wrong3: string = wrongAnswerArray[2];
+
+    createQuizQuestion(flashCard, wrong1, wrong2, wrong3);
+  });
+};
+
+const generateRandom = (num: number) => {
+  return Math.floor(Math.random() * num);
+};
+
+const createQuizQuestion = (
+  flashCard: FlashCard,
+  wrong1: string,
+  wrong2: string,
+  wrong3: string
+) => {
+  let question = {
+    question: flashCard.question,
+    answer: flashCard.answer,
+    wrong1,
+    wrong2,
+    wrong3,
+  };
+  console.log("Generated Question: ", question);
+};
+
+
--- src/components/CreateQuiz/CreateQuiz.tsx
+++ src/components/CreateQuiz/CreateQuiz.tsx
@@ -5,96 +5,13 @@ import {
   isLoaded,
   createQuizState,
 } from "../../StateSlices/CreateQuiz/createQuizSlice";
+import { studySet, studySet2 } from "../../Utilities/dummyData";
+import { createQuiz } from "../../Utilities/createQuiz";
 
 const CreateQuiz = () => {
-  // Create dummy flashcards
-    interface FlashCard{
-
-        id: number,
-        question: string,
-        answer: string,
-    }
-    let flashCard1: FlashCard = {
-    id: 1,
-    question: "Who am I?",
-    answer: "The best",
-  };
-    let flashCard2: FlashCard = {
-    id: 2,
-    question: "what is for lunch?",
-    answer: "burgers",
-  };
-    let flashCard3: FlashCard = {
-    id: 3,
-    question: "Which color?",
-    answer: "green",
-    };
-    let flashCard4: FlashCard = {
-    id:4,
-    question: "What are you wearing, Jake from statefarm?",
-    answer: "Khakis",
-    };
-    
-
-    // Create dummy flashcardSet
-    const studySet = [flashCard1, flashCard2, flashCard3, flashCard4];
-    const studySet2 = [flashCard1, flashCard2, flashCard3, flashCard4];
-    
-    // Q: Which Color?
-    // A. Burgers
-    // B. The Best
-    // C. Green
-    // Correct answer: C
-
-    const createQuiz = (studySet:any) => {
-        //for each card in study set //create quiz questions
-        //callcreateQuizQuestion(flashcard)
-
-        //Generate WRONG ANSWERS BLOCK
-         //string wrongAnswer1 = flashcard[randomnUMgENRATOR].getAnswer()
-         //quizWrongAnswer1. flashcard[randomnUMgENRATOR].answer()
-         //quizWrongAnswer1. flashcard[randomnUMgENRATOR].answer()
-        //createQuizQuestions (currFlashcard,wrongAns1, wrongAns2,wrongAns3)
-
-        let wrong1: string = studySet[Math.floor(Math.random() * studySet.length)].answer;
-        let wrong2: string = studySet[Math.floor(Math.random() * studySet.length)].answer;
-        let wrong3: string = studySet[Math.floor(Math.random() * studySet.length)].answer;
-
-        studySet.forEach((flashCard:FlashCard) => {
-            createQuizQuestion(flashCard, wrong1, wrong2,wrong3);
-        })
-       
-        
-    }
-    const createQuizQuestion = (flashCard:FlashCard, wrong1:string,wrong2:string, wrong3:string) => {
-        let question = {
-            question: flashCard.question,
-            answer: flashCard.answer,
-            wrong1,
-            wrong2,
-            wrong3
-        }
-        
-      }
-    //QuizQuestion: currentFlashcardid.question
-        //quizCorrectAnswer: currentFlashcardid
-        //return quiz questions
-       
-
-    }
-    const randomNumGenerator = (flashcardId) => {
-        //JSRandom generatorF = 0
-        //JSRandom generatorC = studyset.length;
-        //extra logic to exclude currentFLASHCardId
-        
-    }
 
     
-
-    
-
-    const createdQuiz = [[studySet], [studySet2]];
-
+  
   return (
     <Container>
       <Row>
