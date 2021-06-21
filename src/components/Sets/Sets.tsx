d93c49a1f623f26cb8765fd7dda5bd9b1d399023
Vinson <55514029+vinsonchin@users.noreply.github.com>
2021-06-21 15:55:30 -0600

    changed layout of set component and changed some set names

--- src/components/Sets/Sets.tsx
+++ src/components/Sets/Sets.tsx
@@ -1,29 +1,68 @@
-import { Row, Col, Container, ListGroup } from "react-bootstrap";
+import { useState } from "react";
+import { Row, Col, Container, ListGroup, Button, FormControl, Table } from "react-bootstrap";
 import { CardSet } from "../../models/cardSet";
 import { createdSetSearch } from "../../remote/set-service";
 
-const CreatedSets = async () => {
+export function Sets() {
 
-  let accountId = 1;
-  let createdSetElement = await createdSetSearch(accountId);
+  let username = "revature";
+  const [createdSetElement, setCreatedSetElement] = useState(undefined as unknown as CardSet[] || undefined);
+
+  let createdSetsSearch = async (e: any) => {
+    e.preventDefault();
+    const getData = async () => {
+      let response = await createdSetSearch(username);
+      setCreatedSetElement(response.data);
+    };
+    getData();
+  }
+
+  let publicSetsSearch = async (e: any) => {
+
+  } 
   
   return (
-    <Container>
-      <Row>
-        <Col>
-          <ListGroup>
-            {createdSetElement?.map((cardSet: CardSet) => {
-              return (
-                <ListGroup.Item style={{ margin: "10px" }}>
-                  {cardSet.setName}
-                </ListGroup.Item>
-              )
-            })}
-          </ListGroup>
-        </Col>
-      </Row>
-    </Container>
+    <>
+      <Container style={{ marginTop: "10px" }}>
+        <Row className="justify-content-center">
+            <Col className="col-1" style={{ padding: "2px" }}>
+              <Button type="submit" onClick={createdSetsSearch} >Your Sets</Button>
+            </Col>
+            <Col className="col-1" style={{ padding: "2px" }}>
+              <Button type="submit" onClick={publicSetsSearch} >All Public Sets</Button>
+            </Col>
+        </Row>
+      </Container>
+      {
+      createdSetElement
+      &&
+      <Container>
+        <Row>
+          <Col>
+            <Table striped bordered hover size="sm">
+              <thead>
+                <tr>
+                  <th>Set Name</th>
+                  <th>Access</th>
+                </tr>
+              </thead>
+              <tbody>
+              {createdSetElement.map((cardSet: CardSet) => (
+                  <tr>
+                    <td>cardSet.setName</td>
+                    <td>{cardSet.isPublic 
+                    ? "Public"
+                    : "Private"} </td>
+                  </tr>
+              ))}
+              </tbody>
+            </Table>
+          </Col>
+        </Row>
+      </Container>
+      }
+    </>
   );
 };
 
-export default CreatedSets;
+export default Sets;
--- src/remote/set-service.ts
+++ src/remote/set-service.ts
@@ -1,6 +1,6 @@
 import { webappClient } from "./web-client";
 
-export async function createdSetSearch(accountId: number) {
-	let response = await webappClient.get(`/created/${accountId}`);
+export async function createdSetSearch(username: String) {
+	let response = await webappClient.get(`/created/${username}`);
 	return await response.data;
     }
