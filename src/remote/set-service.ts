62a870123ba3772c0730ca19e3cc5212cce2e2f4
annaisha-sys <83236719+annaisha-sys@users.noreply.github.com>
2021-06-22 14:41:30 -0400

    added create studySet

--- src/remote/set-service.ts
+++ src/remote/set-service.ts
@@ -1,5 +1,11 @@
 import { webappClient } from "./web-client";
 
+export async function createStudySet(name: String, isPublic: boolean){
+	let response = await webappClient.post('/sets/newset',{name, isPublic});
+	return await response.data;
+}
+
+
 export async function createdSetSearch(username: String) {
 	let response = await webappClient.get(`/created/${username}`);
 	return await response.data;
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import createQuizReducer from "../StateSlices/CreateQuiz/createQuizSlice";
+import createStudySetReducer from "../StateSlices/StudySets.ts/CreateStudySetsSlice";
 import flashcardsReducer from "../StateSlices/Flashcard/flashcardsSlice";
 import subjectsReducer from "../StateSlices/Subject/subjectsSlice";
 import quizResultsReducer from "../StateSlices/CreateQuiz/resultSlice";
@@ -7,6 +8,7 @@ import quizResultsReducer from "../StateSlices/CreateQuiz/resultSlice";
 export const store = configureStore({
     reducer: {
         createQuiz: createQuizReducer,
+        createStudySet: createStudySetReducer,
         flashcards: flashcardsReducer,
         subjects: subjectsReducer,
         result: quizResultsReducer
