e4710f7121bbd48e09d14843a919029c8fff6c75
DanceableBleats <KylePlummerBSCS@gmail.com>
2021-06-21 21:17:45 -0400

    Prototype jest testing.

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -1,6 +1,5 @@
-import { expect } from "@jest/globals";
-import { test } from "jest-circus";
-import { sum } from "../sum";
+//prototype of a simple jest test. For some reason jest did not like the import command. So we used const importing instead.
+const sum = require('../src/prototypes/sum');
 
 test('Tests addition: ', ()=> {
     expect(sum(1, 2)).toBe(3);
