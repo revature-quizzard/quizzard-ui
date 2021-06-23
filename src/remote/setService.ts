e9aab8dd6668962188d0209711641024cb77a543
Vinson <55514029+vinsonchin@users.noreply.github.com>
2021-06-23 12:51:48 -0600

    added documentation for setService

--- src/remote/setService.ts
+++ src/remote/setService.ts
@@ -1,6 +1,13 @@
 import { quizzardClient } from "./quizzardClient";
 
+/**
+ * An axios get request to get all sets created by account
+ * @param username A string of the username of the account
+ * @returns A list of sets that the account has created
+ * @author Vinson Chin
+ * @author Austin Knauer
+ */
 export async function createdSetSearch(username: String) {
-	let response = await quizzardClient.get(`/created/${username}`);
+	let response = await quizzardClient.get(`/sets/created/${username}`);
 	return await response.data;
     }
