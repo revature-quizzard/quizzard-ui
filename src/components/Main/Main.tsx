a456da4e2da261519c85784fd09797878360dcec
diuguide <everett.diuguid@gmail.com>
2021-06-23 12:39:44 -0400

    added footer to App.tsx, added welcomepage componenet, imported into main, styled welcomepage

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,10 +1,11 @@
 import { Row, Col } from "react-bootstrap";
+import Welcome from "./Welcome";
 
 const Main = () => {
   return (
-    <Row>
-      <Col>
-        <h1>Main COMPONENT</h1>
+    <Row className="d-flex justify-content-center align-items-center">
+      <Col lg={10} style={{ height: "100vh"}}>
+        <Welcome />
       </Col>
     </Row>
   );
---
+++ src/components/Main/Welcome.tsx
@@ -0,0 +1,53 @@
+import { Row, Col } from "react-bootstrap";
+
+const Welcome = () => {
+  return (
+    <>
+      <Row className="d-flex justify-content-center align-items-center mt-4 mb-4">
+        <Col lg={10} className="border bg-dark text-light">
+          <div className="welcomeBanner">
+            Welcome to Qwizzard, your resource for everything QC!
+          </div>
+        </Col>
+      </Row>
+      <Row>
+        <Col className="bg-light">
+          <Row>
+            <Col>
+              <div className="instructionsWelcome">
+                Please login or register using the links in the navigation at
+                the top of the screen to gain access to some exciting features!
+              </div>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <div className="featuresHeader">
+                Features included with this application:
+              </div>
+            </Col>
+          </Row>{" "}
+          <Row>
+            <Col>
+              <div className="featuresList">
+                <ul>
+                  <li>Create your own Flashcards to study with</li>
+                  <li>Add those flashcards to a study set</li>
+                  <li>
+                    Create multiple choice quiz from those study sets to test
+                    your knowledge
+                  </li>
+                  <li>
+                    View other users public study sets to share your knowledge
+                  </li>
+                </ul>
+              </div>
+            </Col>
+                  </Row>
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default Welcome;
