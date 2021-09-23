55846264641322d5acd99836a96b200b883dc9c6
mp34829 <55331051+mp34829@users.noreply.github.com>
2021-09-23 10:01:59 -0700

    Use Wesley's Bookstore as a template

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,6 +1,6 @@
 import { Form, Button, Alert } from "react-bootstrap";
 import { useState } from "react";
-import { login } from "../../remote/login-register-service";
+import { authenticate } from "../../remote/login-register-service";
 import { LoginModel } from "../../models/login-model";
 import {  useDispatch, useSelector } from 'react-redux';
 import { showErrorMessage, hideErrorMessage, errorState } from "../../state-slices/error/errorSlice";
@@ -8,6 +8,7 @@ import { useHistory } from "react-router-dom";
 import { loginUserReducer } from "../../state-slices/auth/auth-slice";
 import {getSubs} from "../../remote/subject-service";
 import {setSubjects} from "../../state-slices/subject/subject-slice";
+import {Redirect} from "react-router-dom";
 
 const Login = () => {
   const dispatch = useDispatch();
@@ -33,25 +34,18 @@ const Login = () => {
 */
   let logUserIn = async (e: any) => {
     e.preventDefault();
-    await login(loginUser).then(response => {
-        localStorage.setItem("Authorization", response.headers.authorization);
-        setLoginUser({username: "", password: ""} as LoginModel);
-        dispatch(loginUserReducer({username: response.data.username, token: response.headers.authorization}));
-        getSubjects();
-        history.push("/study");
-      }).catch(error => {
-        if (error.response.status === 401) {
-          dispatch(showErrorMessage("Invalid Credentials, Please try again!"));
-          setTimeout(() => {
-            dispatch(hideErrorMessage());
-          }, 5000)
-         
-        }
-      }
-    );
+    let response = authenticate(loginUser);
+
+    //localStorage.setItem("Authorization", response.signInUserSession);
+    //setLoginUser({username: "", password: ""} as LoginModel);
+    //dispatch(loginUserReducer({username: response.signInUserSession.accessToken.payload.username, token: response.signInUserSession.accessToken.jwtToken}));
+   // getSubjects();
+    //history.push("/study");
+
   }
 
     return (
+        loginUser ? <Redirect to="/study"/> :
         <>
         <Form>
         <h2>Login</h2>
---
+++ src/config/aws.ts
@@ -0,0 +1,5 @@
+export const COGNITO = {
+    REGION: 'us-west-1',
+    USER_POOL_ID: 'us-west-1_R6GE32PJo',
+    APP_CLIENT_ID: '5s46qlgpsve43hr8rcndana7g'
+}
