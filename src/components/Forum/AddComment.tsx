a6ef3b77586a6abcc57e3a98f4620332f39b758a
LunaHaines <nhaines95@gmail.com>
2021-09-28 14:59:21 -0600

    removed dialog due to issues with tooltip

--- src/components/Forum/AddComment.tsx
+++ src/components/Forum/AddComment.tsx
@@ -1,18 +1,12 @@
 import { useState } from 'react';
 import Editor from 'rich-markdown-editor';
 import Button from '@mui/material/Button';
-import { Dialog } from '@mui/material';
 
-interface IAddCommentProps {
-    open: boolean;
-    setOpen: (openStatus: boolean) => void
-}
-
-function AddComment(props: IAddCommentProps) {
+function AddComment() {
     const [inputText, setInputText] = useState('');
 
     let handleChange = (e: any) => {
-        setInputText(e);
+        setInputText(e)
     }
 
     let handleClick = async () => {
@@ -24,15 +18,11 @@ function AddComment(props: IAddCommentProps) {
         }
     }
 
-    let handleClose = () => {
-        props.setOpen(false);
-    }
-
     return (
-        <Dialog open={props.open} onClose={handleClose} style={{'margin': '2rem'}}>
+        <div style={{'margin': '2rem'}}>
             <Editor onChange={handleChange} placeholder='Write your comment here...' />
             <Button onClick={handleClick}>Create</Button>
-        </Dialog>
+        </div>
     )
 }
 
