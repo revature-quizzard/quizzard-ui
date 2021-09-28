83440bd6aaeabb6dbcc9845b8860ba11e5c287a1
LunaHaines <nhaines95@gmail.com>
2021-09-28 14:21:26 -0600

    AddComment component

---
+++ src/components/Forum/AddComment.tsx
@@ -0,0 +1,11 @@
+import Editor from 'rich-markdown-editor';
+
+function AddComment() {
+    return (
+        <>
+            <Editor placeholder='Write your comment here...' />
+        </>
+    )
+}
+
+export default AddComment;
