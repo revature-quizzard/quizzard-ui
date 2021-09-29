77eebe1179c1dd38e67cb6c95f7e35dec557fdd0
LunaHaines <nhaines95@gmail.com>
2021-09-29 13:32:12 -0600

    added forum state to add comment

--- src/components/Forum/AddComment.tsx
+++ src/components/Forum/AddComment.tsx
@@ -4,10 +4,12 @@ import { useSelector } from 'react-redux';
 import Editor from 'rich-markdown-editor';
 import Button from '@mui/material/Button';
 import { Paper } from '@mui/material';
+import { forumState } from '../../state-slices/forum/forum-slice';
 
 function AddComment() {
     const [inputText, setInputText] = useState('');
     const auth = useSelector(authState);
+    const forumInfo = useSelector(forumState);
 
     let handleChange = (e: any) => {
         setInputText(e)
