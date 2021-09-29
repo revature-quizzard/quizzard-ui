0f1dbed6dedc1ea62e3bfb03adaa655a028cb827
Sean Dunn <60554213+spdunn@users.noreply.github.com>
2021-09-29 15:57:24 -0400

    Added testing breadcrumbs for game container and game settings
    Signed-off-by: Sean Dunn <60554213+spdunn@users.noreply.github.com>

---
+++ src/__tests__/multiplayer-tests/game.test.tsx
@@ -0,0 +1,46 @@
+import {shallow, mount} from 'enzyme';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
+import Game from '../../components/Multiplayer/Game';
+
+describe('GameContainer Test Suite', () => {
+
+    afterEach(() => {
+        jest.resetAllMocks();
+    });
+
+    it('Game container renders successfully', () => {
+        const wrapper = shallow(<Game />);
+
+        expect(wrapper).toBeTruthy;
+    });
+
+    it('Game Container renders splash screen when no game exists', () => {
+        const wrapper = mount(<Game />);
+
+        expect(wrapper).toBeTruthy;
+    });
+
+    it('Game Container renders correctly when match state is 0', () => {
+        const wrapper = mount(<Game />);
+
+        expect(wrapper).toBeTruthy;
+    });
+
+    it('Game Container renders correctly when match state is 1', () => {
+        const wrapper = mount(<Game />);
+
+        expect(wrapper).toBeTruthy;
+    });
+
+    it('Game Container renders correctly when match state is 2', () => {
+        const wrapper = mount(<Game />);
+
+        expect(wrapper).toBeTruthy;
+    });
+
+    it('Game Container renders correctly when match state is 3', () => {
+        const wrapper = mount(<Game />);
+
+        expect(wrapper).toBeTruthy;
+    });
+})
