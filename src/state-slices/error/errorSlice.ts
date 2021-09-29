fb484b213a06ed1dc9225c11d7244acfd21dabf4
LunaHaines <nhaines95@gmail.com>
2021-09-29 14:36:48 -0600

    updated error slice for snackbar move

--- src/state-slices/error/errorSlice.ts
+++ src/state-slices/error/errorSlice.ts
@@ -1,14 +1,22 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { RootState } from "../../store/store";
+import { AlertColor } from '@mui/material';
 
 interface State {
     errorMsg: string;
     showError: boolean;
+    errorSeverity: AlertColor;
 }
 
 const initialState: State = {
     errorMsg: "",
     showError: false,
+    errorSeverity: 'info'
+}
+
+class errorInput {
+    message: string;
+    severity: AlertColor;
 }
 
 export const errorSlice = createSlice({
@@ -16,12 +24,18 @@ export const errorSlice = createSlice({
     initialState,
     reducers: {
         showErrorMessage: (state, action: PayloadAction<string>) => {
-            state.errorMsg = action.payload;
+            state.errorMsg = action.payload; 
+            state.showError = true;
+        },
+        showSnackbar: (state, action: PayloadAction<errorInput>) => {
+            state.errorMsg = action.payload.message;
+            state.errorSeverity = action.payload.severity;
             state.showError = true;
         },
         hideErrorMessage: (state) => {
             state.errorMsg = "";
             state.showError = false;
+            state.errorSeverity = 'info';
         }
     }
 });
