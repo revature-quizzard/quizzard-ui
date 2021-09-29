f0504b8d73a76fd028da3e4107480b60673298f0
hguilfoyle <hguilfoyle1@gmail.com>
2021-09-29 15:00:11 -0400

    answer breadcrumbs
    Signed-off-by: hguilfoyle <hguilfoyle1@gmail.com>

--- src/components/Multiplayer/Answers.tsx
+++ src/components/Multiplayer/Answers.tsx
@@ -1,1 +1,59 @@
-export default {}
+import { Table, Button, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@material-ui/core';
+import { Row } from 'react-bootstrap';
+
+
+/**
+ * React component that renders that chooseable answers for the questions to players.
+ * Function needed to pull random answers off of cards in the set (including correct answer).
+ *
+ * State 1
+ * Function submit 
+ * Function Check answer 
+ * 
+ * State 2
+ * Function needed to display correct or incorrect answer.
+ * 
+ * @authors Heather Guilfoyle, Sean Dunn, Colby Wall
+ */
+
+function generateAnswers(): string[] {
+ //pull answer from current question +
+ //pull 3 random answers from the set
+ //randomize them
+ return []
+}
+
+function submit() {
+    
+}
+
+function renderColors() {
+    
+}
+
+
+function Answers() {
+
+    let answers = generateAnswers()
+    // if redux.state-slices.store.game.match_state == 2 renderColors()
+    return (
+        <>
+        <TableContainer>
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <TableCell id= "answer1" onClick={submit}>{answers[0]}</TableCell>
+                        <TableCell id= "answer2" onClick={submit}>{answers[1]}</TableCell>
+                    </TableRow>
+                    <TableRow>
+                        <TableCell id= "answer3" onClick={submit}>{answers[2]}</TableCell>
+                        <TableCell id= "answer4" onClick={submit}>{answers[3]}</TableCell>
+                    </TableRow>
+                </TableHead>
+            </Table>
+        </TableContainer>
+        </>
+
+    );
+}
+export default Answers; 
