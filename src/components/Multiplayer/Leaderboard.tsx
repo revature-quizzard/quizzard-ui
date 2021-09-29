60e3ac108e38a9a2955d48afdd6a60b2ebe47028
Sean Dunn <60554213+spdunn@users.noreply.github.com>
2021-09-29 12:03:46 -0400

    Leaderboard Breadcrumbs

--- src/components/Multiplayer/Leaderboard.tsx
+++ src/components/Multiplayer/Leaderboard.tsx
@@ -1,1 +1,26 @@
-export default {}
+
+/*  
+    The Leaderboard will need to be passed a presorted list of players, which
+    contains usernames and points.
+    The Leaderboard may potentially be passed additional information such as game 
+    length, statistics, etc.
+ */
+interface ILeaderboardProps {
+    // players: Player[]
+}
+
+/**
+ *  The leaderboard component will display a ranked list of players from the game
+ */
+function Leaderboard(props: ILeaderboardProps) {
+
+    return (
+        <>
+            {/* <Table>
+                props.players.map(player.name | player.points)
+            </Table> */}
+        </>
+    )
+}
+
+export default Leaderboard
