1eb66daa97cce5a6604c132ac255c81fe67e6a4a
ColbyDub <cduball@outlook.com>
2021-09-29 10:50:58 -0500

    Question breadcrumb with mui and removed unused dependency

--- src/components/Multiplayer/Questions.tsx
+++ src/components/Multiplayer/Questions.tsx
@@ -1,9 +1,35 @@
- function Questions() {
+import{ Card, CardContent, Typography }from '@material-ui/core';
 
+/**
+ * React component that renders a questions to the players
+ * Props: the question, current index, the total number of cards
+ * 
+ * @author Colby Wall, Sean Dunn, Heather Guilfoyle
+ */
+
+interface IQuestionProps {
+    question: string,
+    index: number,
+    total: number
+}
+
+ function Questions(props: IQuestionProps) {
+ 
     return (
         <>
+            <Card>
+                <CardContent>
+                    <Typography variant="h5" gutterBottom>
+                     {props.question}
+                    </Typography>
+ 
+                    <Typography >  {/*sx={{ mb: 1.5 }}*/}
+                    Question {props.index} of {props.total}
+                    </Typography>
+                </CardContent>
+            </Card>
         </>
     );
  }
-
+ 
 export default Questions
