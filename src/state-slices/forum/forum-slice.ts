a9b19a97cf9ed4ff0d1892627dbfe7271909443c
LunaHaines <nhaines95@gmail.com>
2021-09-29 13:16:30 -0600

    create slice, make reducers

--- src/state-slices/forum/forum-slice.ts
+++ src/state-slices/forum/forum-slice.ts
@@ -1,3 +1,4 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { Subforum } from '../../models/subforum';
 import { Thread } from '../../models/thread';
 
@@ -11,3 +12,16 @@ const initialState: State = {
     currentSubforum: undefined,
     currentThread: undefined
 }
+
+export const forumSlice = createSlice({
+    name: 'forum',
+    initialState,
+    reducers: {
+        setCurrentSubforum: (state, action: PayloadAction<Subforum>) => {
+            state.currentSubforum = action.payload;
+        },
+        setCurrentThread: (state, action: PayloadAction<Thread>) => {
+            state.currentThread = action.payload;
+        }
+    }
+})
