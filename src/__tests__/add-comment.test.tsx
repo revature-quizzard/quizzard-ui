1d527cff35db577972a63a306d82823da3b3f7c0
LunaHaines <nhaines95@gmail.com>
2021-09-30 14:51:35 -0600

    added initial state to before and after each

--- src/__tests__/add-comment.test.tsx
+++ src/__tests__/add-comment.test.tsx
@@ -1,12 +1,34 @@
-import { shallow } from 'enzyme';
+import { shallow, configure } from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import AddComment from '../components/Forum/AddComment';
+import { Subforum } from '../models/subforum';
+import { Thread } from '../models/thread';
 import { addComment } from '../remote/comment-service';
 jest.mock('../remote/comment-service');
 
+interface TempState {
+    currentSubforum: Subforum | undefined;
+    currentThread: Thread | undefined;
+}
+
+let initialState: TempState
+
 describe('Add Comment Component Test Suite', () => {
+
+    beforeAll(() => {
+        configure({ adapter: new Adapter() });
+    })
+
+    beforeEach(() => {
+        initialState = {
+            currentSubforum: undefined,
+            currentThread: undefined
+        }
+    })
 
     afterEach(() => {
         jest.resetAllMocks();
+        initialState = null;
     })
 
     it('Component renders successfully', () => {
@@ -19,7 +41,7 @@ describe('Add Comment Component Test Suite', () => {
 
     it('Component calls addComment (POST request to quizzard api) when given valid information', () => {
         // configure mock redux store
-        
+        // adding something here
     })
 
     
