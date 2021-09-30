03f6d45a0f8b8cb24bd214af6132b83ef75c8951
LunaHaines <nhaines95@gmail.com>
2021-09-30 14:39:56 -0600

    wrote base test - component renders

--- src/__tests__/add-comment.test.tsx
+++ src/__tests__/add-comment.test.tsx
@@ -1,6 +1,26 @@
+import { shallow } from 'enzyme';
+import AddComment from '../components/Forum/AddComment';
 import { addComment } from '../remote/comment-service';
 jest.mock('../remote/comment-service');
 
 describe('Add Comment Component Test Suite', () => {
 
+    afterEach(() => {
+        jest.resetAllMocks();
+    })
+
+    it('Component renders successfully', () => {
+        // set up wrapper class
+        const wrapper = shallow(<AddComment />);
+
+        // expect it to be truthy (i.e. something renders)
+        expect(wrapper).toBeTruthy();
+    })
+
+    it('Component calls addComment (POST request to quizzard api) when given valid information', () => {
+        // configure mock redux store
+        
+    })
+
+    
 })
