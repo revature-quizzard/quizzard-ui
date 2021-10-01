2c60d434743d6fd859d4127781c7d78a154d14e2
metteec2 <chmettee@gmail.com>
2021-10-01 12:08:14 -0400

    implemented AddThread component
    Signed-off-by: metteec2 <chmettee@gmail.com>

---
+++ src/components/Forum/AddThread.tsx
@@ -0,0 +1,96 @@
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import Editor from 'rich-markdown-editor';
+import { Paper } from '@mui/material';
+import { Button, Box, FormControl, Input, Typography, makeStyles } from '@material-ui/core';
+import { authState } from '../../state-slices/auth/auth-slice';
+import { forumState } from '../../state-slices/forum/forum-slice';
+import {Thread } from '../../models/thread';
+import { addThread } from '../../remote/thread-service';
+
+const useStyles = makeStyles({
+    addThreadContainer: {
+        justifyContent: "center",
+        margin: 'auto',
+        padding: 20,
+        width: "50%",
+        borderStyle: 'solid',
+        borderColor: '#333333',
+        backgroundColor: '#5E5E5E',
+        color: '#FFFFFF',
+    },
+    input: {
+        backgroundColor: '#FFFFFF'
+    },
+    button: {
+        color: '#333333',
+        backgroundColor: '#FFFFFF',
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+});
+
+function AddThread() {
+    const classes = useStyles();
+    const [description, setDescription] = useState('');
+    const [subject, setSubject] = useState('');
+    const auth = useSelector(authState);
+    const forumInfo = useSelector(forumState);
+
+    
+    let handleSubjectChange = (e: any) => {
+        setSubject(e);
+    }
+
+    let handleDescriptionChange = (e: any) => {
+        setDescription(e);
+    }
+
+    let handleClick = async () => {
+        try {
+            let threadAncestors: string[] = [forumInfo.currentSubforum.id];
+            let toAdd = new Thread(
+                threadAncestors,
+                forumInfo.currentSubforum.id,
+                description,
+                subject,
+                0,
+                auth.username,
+            );
+            let resp = await addThread(toAdd);
+        } catch (e: any) {
+            console.log(e);
+            // #TODO: set error message / toast here
+        }
+    }
+
+
+    return (
+        <div id="add-thread-component" className={classes.addThreadContainer}>
+                <Typography align="center" variant="h5">Create Thread</Typography>
+                <br />
+                <Typography>Thread Title</Typography>
+                <FormControl style={{'margin': '1rem'}}>
+                    <Input className={classes.input}
+                        onChange={handleSubjectChange}
+                        placeholder="Enter the thread title"
+                        fullWidth
+                    />
+                </FormControl>
+
+                <Typography>Enter Your Post</Typography>
+                <Paper style={{'margin': '1rem'}}>
+                    <Editor
+                        onChange={handleDescriptionChange}
+                        placeholder='Enter the first post here...' />
+                </Paper>
+                <br />
+                <Box textAlign='center'>
+                    <Button variant="contained" className={classes.button} onClick={handleClick}>Create Thread</Button>
+                </Box>
+        </div>
+    )
+
+}
+
+export default AddThread;
