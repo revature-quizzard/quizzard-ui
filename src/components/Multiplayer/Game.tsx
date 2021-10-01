23758072c8f927df7d3cb43de311a93b0378411d
heraldOfMechanus <jccallahan11@gmail.com>
2021-10-01 11:52:35 -0500

    Altered the formatting of the users list to make it more purple. Clearly a great thing

--- src/components/Multiplayer/Game.tsx
+++ src/components/Multiplayer/Game.tsx
@@ -1,6 +1,4 @@
 import { useState, useEffect, useRef } from 'react';
-import { useDispatch, useSelector } from "react-redux";
-import { gameState, setGame } from '../../state-slices/multiplayer/game-slice';
 
 import Amplify, { API, graphqlOperation } from 'aws-amplify';
 import config from '../../aws-exports';
@@ -127,30 +125,11 @@ function render() {
 
 function Game() {
 
-    const game = useSelector(gameState);
-    const dispatch = useDispatch();
-
     // TODO: Change to be actual values
     let dummyGameId = 1;
-    // let dummyGame = undefined;
-    let dummyGame = {
-        id: '13',
-        name: '',
-        match_state: 0,
-        question_index: 0,
-        capacity: 0,
-        set: {
-            //@ts-ignore
-            card_list: []
-        },
-        //@ts-ignore
-        players: []
-    }
+    let dummyGame = undefined;
 
     useEffect(() => {
-
-        dispatch(setGame(dummyGame));
-
         // Subscribe to changes in current game in DynamoDB
         const updateSubscription = (API.graphql(
             graphqlOperation(onUpdateGameById, {id: dummyGameId})
@@ -168,37 +147,16 @@ function Game() {
         }
     }, [])
 
-    function test(game: any) {
-        let newgame = {
-            id: game.id,
-            name: '',
-            match_state: 0,
-            question_index: 0,
-            capacity: 0,
-            set: {
-                //@ts-ignore
-                card_list: []
-            },
-            //@ts-ignore
-            players: []
-        }
-        newgame.id = parseInt(game.id) + 1;
-        return newgame;
-    }
-
     // The return renders components based on match state if game exists in redux,
     // otherwise, redirect user to game lounge
     return (
         <>
-        <h1>{game.id}</h1>
-        <Button onClick={() => dispatch(setGame(test(game)))}>Click Me</Button>
         {
-            console.log(game)
-            // (game.id != '-1') // If game is defined (Using redux slice)
+            // (dummyGame) // If game is defined (Using redux slice)
             // ?
-            // <>
-            //     { render() }
-            // </>            
+            <>
+                { render() }
+            </>            
             // : <Redirect to="lounge" />
         }
         </>
