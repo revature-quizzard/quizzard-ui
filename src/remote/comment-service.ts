62690ba17bf0eab8b48a3eff4cf9e2df58697e99
metteec2 <chmettee@gmail.com>
2021-10-02 19:01:09 -0400

    Updated documentation
    Signed-off-by: metteec2 <chmettee@gmail.com>

--- src/remote/comment-service.ts
+++ src/remote/comment-service.ts
@@ -19,7 +19,8 @@ export async function addComment(comment: Comment) {
 /**
  * async function which makes an axios call to the API to replace an existing comment with an updated version of itself
  * @param comment - The comment to be updated in the database 
- * @returns The data inside the axios response, including a 200 status code
+ * @returns - The data inside the axios response, including a 200 status
+ * @author - Charles Mettee
  */
 export async function updateComment(comment: Comment) {
     let response = await QuizzardClient.put('/forum/comment', comment);
