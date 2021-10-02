8fb322d5a6b96cc58394e6d08f685a50e4ef9a85
Hiroshi Nobuoka <hiroshi.nobuoka@gmail.com>
2021-10-01 20:05:45 -0400

    comments, but no rendering

--- src/components/Forum/ViewComments.tsx
+++ src/components/Forum/ViewComments.tsx
@@ -2,14 +2,19 @@ import { useSelector } from 'react-redux';
 import { forumState } from '../../state-slices/forum/forum-slice';
 import { viewComments } from '../../remote/comment-service';
 import { Paper } from '@mui/material';
+import { Comment } from '../../models/comment';
 import { Divider, Avatar, Grid } from "@material-ui/core";
 import React from 'react';
 
 
 function ViewComment() {
     const forumInfo = useSelector(forumState);
-
-    let resp;
+    let testArray = [
+        new Comment(["ancestor1", "ancestor2"], "parent1ID", "This is a comment!", "hax0r", "ID1", "Subject1", 0, "dateCreated"),
+        new Comment(["ancestor1", "ancestor2"], "parent2ID", "This is another comment!", "LukesUsername", "ID2", "Subject1", 0, "dateCreated"),
+        new Comment(["ancestor1", "ancestor2"], "parent3ID", "This is a third comment!", "negativeNancy", "ID3", "Subject1", 0, "dateCreated")
+    ];
+    let resp: Comment[] | undefined;
     let handleClick = async () => {
         try{
             resp = await viewComments(forumInfo.currentThread.id);
@@ -18,35 +23,45 @@ function ViewComment() {
         }
     }
 
-return (
-    <>
-        <Paper elevation={3} style={{'margin':'4rem'}}>
-            <div style={{'margin': '2rem'}}>
-                <h1>Thread subject</h1>
-                <br></br>
-                <br></br>
-                <p>Thread description</p>
-                {/* <h1>forumInfo.currentThread.subject</h1>
-                <p>forumInfo.currentThread.description</p> */}
-            </div>
-        </Paper>
-        {true && 
-            <Paper style={{ padding: "40px 20px" }}>
-                <Grid container wrap="nowrap" spacing={2}>
-                    <Grid item xs zeroMinWidth>
-                    <h5 style={{ margin: 0, textAlign: "left" }}>USERNAME</h5>
-                    <p style={{ textAlign: "left" }}>
-                        Comment!
-                    </p>
-                    <p style={{ textAlign: "left", color: "gray" }}>
-                        posted 1 minute ago
-                    </p>
-                    </Grid>
-                </Grid>
+    return (
+        <>
+            <Paper elevation={3} style={{ padding: "40px 20px"}}>
+            
+                <div style={{'margin': '2rem'}}>
+                    <h1>Thread subject</h1>
+                    <br></br>
+                    <br></br>
+                    <p>Thread description</p>
+                    {/* <h1>forumInfo.currentThread.subject</h1>
+                    <p>forumInfo.currentThread.description</p> */}
+                </div>    
             </Paper>
-        }
-    </>
-    
-)
+
+             <br></br>
+
+           
+            {testArray.map( (comment : Comment) => {
+                <>
+                    <Paper style={{ padding: "40px 20px" }}>
+                        <Grid container wrap="nowrap" spacing={2}>
+                            <Grid item xs zeroMinWidth>
+                            <h5 style={{ margin: 0, textAlign: "left" }}>comment.username</h5>
+                            <p style={{ textAlign: "left" }}>
+                                comment.description
+                            </p>
+                            <p style={{ textAlign: "left", color: "gray" }}>
+                                comment.date_created
+                            </p>
+                            </Grid>
+                        </Grid>
+                    </Paper>
+                    <Divider variant="fullWidth" style={{ margin: "30px 0" }} />
+                </>
+            }) 
+            
+            }
+        </>
+    )
 }
+
 export default ViewComment;
