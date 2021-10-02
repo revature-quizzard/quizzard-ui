058e324ee691a0ec2419d287ec5abaa35eb7c10d
heraldOfMechanus <jccallahan11@gmail.com>
2021-10-01 21:05:59 -0500

    Stored the game received from graphQL and redirect the user to their game page.

--- src/components/Multiplayer/GameLounge.tsx
+++ src/components/Multiplayer/GameLounge.tsx
@@ -1,28 +1,39 @@
 import { useState, useEffect, useRef } from 'react';
 import { useDispatch, useSelector } from "react-redux";
 import { gameState, setGame } from '../../state-slices/multiplayer/game-slice';
-
 import { Button, Input } from '@material-ui/core';
 import { Link, Redirect } from 'react-router-dom';
-import { getGame } from '../../graphql/queries';
+import { gameByName, getGame } from '../../graphql/queries';
+import Amplify, { API, graphqlOperation } from 'aws-amplify';
+import { GraphQLResult } from '@aws-amplify/api-graphql';
+import config from '../../aws-exports';
+
+Amplify.configure(config);
 
 /** This React component is a splash screen/landing page for the multiplayer quiz game.
  * 
  *  If no game is currently defined, a lobby will be rendered which allows users to 
  *      define game settings and create a new game or join an existing game by ID.
  * 
- *  @author Sean Dunn, Colby Wall, Heather Guilfoyle
+ *  @author Sean Dunn, Colby Wall, Heather Guilfoyle, John Callahan
  **/
-
-function joinGame(id: string) {
-    // let game = getGame(id); FIND THE GRAPH QL NOTATION!
-
-}
 
 function GameLounge() {
 
     const game = useSelector(gameState);
     const dispatch = useDispatch();
+    let id = useRef('');
+    
+    async function fetchGame() {
+        console.log(id.current);
+        let resp = await (API.graphql(graphqlOperation(getGame, {id: id.current})) as Promise<GraphQLResult>)
+        dispatch(setGame(resp.data));
+    }
+    
+    function handleUpdate(e: any) {
+        id.current = e.target.value;
+        console.log(id);
+    }
 
     return (
         <>
@@ -44,10 +55,10 @@ function GameLounge() {
         {/* <Button >Create Game</Button> */}
 
         {/* Input field for the join game ID */}
-        <Input />
+        <Input onKeyUp={handleUpdate} />
 
         {/* Button which joins existing game according to input id */}
-        <Button >Join Game</Button>
+        <Button onClick={fetchGame}>Join Game</Button>
         <Link to="/multiplayer">Go Back To Multiplayer</Link> 
         </>
         : <Redirect to="/multiplayer" /> }
