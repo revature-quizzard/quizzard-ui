b43cba7bf92859648d067256fc95afc8379dec71
LunaHaines <nhaines95@gmail.com>
2021-10-04 10:15:03 -0600

    added editing boolean to state

--- src/state-slices/forum/forum-slice.ts
+++ src/state-slices/forum/forum-slice.ts
@@ -9,12 +9,14 @@ interface State {
     currentSubforum: Subforum | undefined;
     currentThread: Thread | undefined;
     currentComment: Comment | undefined;
+    isEditing: boolean;
 }
 
 const initialState: State = {
     currentSubforum: undefined,
     currentThread: undefined,
-    currentComment: undefined
+    currentComment: undefined,
+    isEditing: false
 }
 
 export const forumSlice = createSlice({
@@ -29,11 +31,14 @@ export const forumSlice = createSlice({
         },
         setCurrentComment: (state, action: PayloadAction<Comment>) => {
             state.currentComment = action.payload;
+        },
+        setEditing: (state, action: PayloadAction<boolean>) => {
+            state.isEditing = action.payload;
         }
     }
 })
 
-export const { setCurrentSubforum, setCurrentThread, setCurrentComment } = forumSlice.actions;
+export const { setCurrentSubforum, setCurrentThread, setCurrentComment, setEditing } = forumSlice.actions;
 
 export const forumState = (state: RootState) => state.forum;
 
