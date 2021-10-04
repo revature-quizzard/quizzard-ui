cc22ee40fc2595fc51f6d215723e2ea4b62e8e9d
hpuf <csmcdn@gmail.com>
2021-10-04 11:39:55 -0500

    Now cleanly displaying user set/favorite data

--- src/components/UserProfile/UserFavoriteSets.tsx
+++ src/components/UserProfile/UserFavoriteSets.tsx
@@ -8,46 +8,49 @@ import {SetDocument} from "../../models/set-document";
 /**
  * Component for rendering a user's Favorite sets.
  * Each set will have a link to the corresponding set and a button to remove the set from favorites.
- * @author Cody McDonald
+ * @author Cody McDonald, Mitchell Panenko
  * */
 
 const UserFavoriteSets = () => {
     const state = useSelector(profileState);
     const userFavorites = state.userProfile.favoriteSets;
     const columns = [
-        { field: 'setName', headerName: 'Name', width: 120},
-        { field: 'tags', headerName: 'Tags', width: 150},
-        { field: 'isPublic', headerName: 'Public?', width: 100},
-        { field: 'views', headerName: 'Views', width: 100},
-        { field: 'plays', headerName: 'Plays', width: 100},
-        { field: 'studies', headerName: 'Studies', width: 100},
-        { field: 'favorites', headerName: 'Faves', width: 120}
+        {field: 'setName', headerName: 'Name', flex: 1},
+        {field: 'author', headerName: 'Author', flex: 1},
+        {field: 'tags', headerName: 'Tags', flex: 1},
+        {field: 'isPublic', headerName: 'Public?', flex: 1},
+        {field: 'views', headerName: 'Views', flex: 1},
+        {field: 'plays', headerName: 'Plays', flex: 1},
+        {field: 'studies', headerName: 'Studies', flex: 1},
+        {field: 'favorites', headerName: 'Faves', flex: 1}
     ]
 
-    const rows = userFavorites.map((set:SetDocument, index:any) => (
-        {
-            id: index,
-            setName: set.setName,
-            tags: set.tags,
-            isPublic: set.isPublic,
-            views: set.views,
-            plays: set.plays,
-            studies: set.studies,
-            favorites: set.favorites
-        }
+    const rows = userFavorites.map((set: SetDocument, index: any) => (
+            {
+                id: set.id,
+                index: index,
+                setName: set.setName,
+                author: set.author,
+                tags: set.tags.map((x)=>(x.tagName)),
+                isPublic: set.isPublic,
+                views: set.views,
+                plays: set.plays,
+                studies: set.studies,
+                favorites: set.favorites
+            }
         )
-
     )
 
     console.log(rows);
 
     return (
         <>
-            <Container fixed maxWidth='md' id='register-component' sx={{height:'400px'}}>
                 {userFavorites.length ?
                     <DataGrid
                         columns={columns}
                         rows={rows}
+                        autoHeight={true}
+                        disableExtendRowFullWidth={true}
                         pagination={true}
                         pageSize={5}
                         rowsPerPageOptions={[5]}
@@ -55,10 +58,9 @@ const UserFavoriteSets = () => {
                     :
                     <Typography>
                         You haven't added any sets to your favorites yet!<br/>
-                        Go to the <Link to="/sets">Sets Page</Link> to find some!
+                        Go to the <Link to="/study">Discovery Page</Link> to find some!
                     </Typography>
                 }
-            </Container>
         </>
     )
 };
--- src/components/UserProfile/UserSets.tsx
+++ src/components/UserProfile/UserSets.tsx
@@ -1,35 +1,62 @@
 import {Container, Typography} from "@mui/material";
 import {useSelector} from "react-redux";
 import {profileState} from "../../state-slices/user-profile/profile-slice";
+import {DataGrid} from "@mui/x-data-grid";
+import {SetDocument} from "../../models/set-document";
 
 /**
  * Component for rendering a user's Sets.
  * Will contain a button to render a modal for adding sets.
  * Each set will have a button for editing or deleting the corresponding set and a link to the set.
- * @author Cody McDonald
+ * @authors Cody McDonald, Mitchell Panenko
  * */
 
 const UserSets = () => {
     const state = useSelector(profileState);
     const userCreatedSets = state.userProfile.createdSets;
 
+    const columns = [
+        {field: 'setName', headerName: 'Name', flex: 1},
+        {field: 'tags', headerName: 'Tags', flex: 1},
+        {field: 'isPublic', headerName: 'Public?', flex: 1},
+        {field: 'views', headerName: 'Views', flex: 1},
+        {field: 'plays', headerName: 'Plays', flex: 1},
+        {field: 'studies', headerName: 'Studies', flex: 1},
+        {field: 'favorites', headerName: 'Faves', flex: 1}
+    ]
+
+    const rows = userCreatedSets.map((set: SetDocument, index: any) => (
+            {
+                id: index,
+                setName: set.setName,
+                tags: set.tags.map((x)=>(x.tagName)),
+                isPublic: set.isPublic,
+                views: set.views,
+                plays: set.plays,
+                studies: set.studies,
+                favorites: set.favorites
+            }
+        )
+    )
 
     return (
         <>
-            <Container fixed maxWidth='md' id='register-component'>
                 <br/><br/>
                 {userCreatedSets.length ?
-                    <Typography>
-                        MySets : {JSON.stringify(userCreatedSets)}<br/>
-                    </Typography>
+                    <DataGrid
+                        columns={columns}
+                        rows={rows}
+                        autoHeight={true}
+                        disableExtendRowFullWidth={true}
+                        pagination={true}
+                        pageSize={5}
+                        rowsPerPageOptions={[5]}
+                    />
                     :
-                    // TODO: Hook this up when create set component is made
                     <Typography>
-                        You haven't created any sets!
-                        Click here to create your first set!
+                        You haven't created any sets yet!
                     </Typography>
                 }
-            </Container>
         </>
     )
 };
