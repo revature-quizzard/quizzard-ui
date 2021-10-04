69408d117e5928eefc443e12ef3ff49600985538
heraldOfMechanus <jccallahan11@gmail.com>
2021-10-04 10:04:46 -0500

    conditionally rendered a kick button for the host. updated the graphql for removing a kicked player from the players list.

--- src/components/Multiplayer/Players.tsx
+++ src/components/Multiplayer/Players.tsx
@@ -1,7 +1,11 @@
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, useTheme } from '@material-ui/core';
+import API from '@aws-amplify/api';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, useTheme, Button } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core';
+import { graphqlOperation } from 'aws-amplify';
 import { useSelector } from 'react-redux';
-import { gameState } from '../../state-slices/multiplayer/game-slice';
+import { deleteGame, updateGame } from '../../graphql/mutations';
+import { authState } from '../../state-slices/auth/auth-slice';
+import { gameState, Player } from '../../state-slices/multiplayer/game-slice';
 
 /**
  * React component that renders the current players username and their points of an active game.
@@ -28,6 +32,9 @@ import { gameState } from '../../state-slices/multiplayer/game-slice';
       },
       text: {
           color: 'white'
+      },
+      button: {
+          color: 'red'
       }
   }))
   
@@ -35,6 +42,16 @@ function Players() {
 
     const styles = useStyles();
     const game = useSelector(gameState);
+    const user = useSelector(authState);
+
+    const executeKick = async (user: Player) => {
+        let copylist = [].concat(game.players);
+        let index = copylist.find((player) => {
+            player.id == user.id;
+        })
+        copylist.splice(index, 1);
+        (API.graphql(graphqlOperation(updateGame, {input: {id: game.id, players: copylist}})));
+    }
 
     return (
         <>
@@ -47,14 +64,27 @@ function Players() {
                 </TableRow>
                 </TableHead>
                 <TableBody>
-                {game.players.map((player) => (
+                {user.username == game.host
+                ?
+                game.players.map((player) => (
                     <TableRow
                     key={player.username}      
                     >
                     <TableCell className={styles.text} align="left">{player.username}</TableCell>
                     <TableCell className={styles.text} align="right">{player.points}</TableCell>
+                    <TableCell className={styles.text} align="right">Kick Players</TableCell>
                     </TableRow>
-                ))}
+                ))
+                :
+                game.players.map((player) => (
+                    <TableRow
+                    key={player.username}      
+                    >
+                    <TableCell className={styles.text} align="left">{player.username}</TableCell>
+                    <TableCell className={styles.text} align="right">{player.points}</TableCell>
+                    <TableCell className={styles.button} align="right"><Button onClick={() => executeKick(player)}>Kick</Button></TableCell>
+                    </TableRow>                
+                    ))}
                 </TableBody>
             </Table>
             </TableContainer> 
