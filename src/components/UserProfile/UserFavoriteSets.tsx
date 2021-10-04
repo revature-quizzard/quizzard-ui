4289d7bc082170b2662babb743eb8248f8fe8071
hpuf <csmcdn@gmail.com>
2021-10-04 10:39:55 -0500

    begun setting up datagrid to display set data

--- src/components/UserProfile/UserFavoriteSets.tsx
+++ src/components/UserProfile/UserFavoriteSets.tsx
@@ -2,6 +2,8 @@ import {Container, Typography} from "@mui/material";
 import {useSelector} from "react-redux";
 import {profileState} from "../../state-slices/user-profile/profile-slice";
 import {Link} from "react-router-dom";
+import {DataGrid} from "@mui/x-data-grid";
+import {SetDocument} from "../../models/set-document";
 
 /**
  * Component for rendering a user's Favorite sets.
@@ -12,14 +14,44 @@ import {Link} from "react-router-dom";
 const UserFavoriteSets = () => {
     const state = useSelector(profileState);
     const userFavorites = state.userProfile.favoriteSets;
+    const columns = [
+        { field: 'setName', headerName: 'Name', width: 120},
+        { field: 'tags', headerName: 'Tags', width: 150},
+        { field: 'isPublic', headerName: 'Public?', width: 100},
+        { field: 'views', headerName: 'Views', width: 100},
+        { field: 'plays', headerName: 'Plays', width: 100},
+        { field: 'studies', headerName: 'Studies', width: 100},
+        { field: 'favorites', headerName: 'Faves', width: 120}
+    ]
+
+    const rows = userFavorites.map((set:SetDocument, index:any) => (
+        {
+            id: index,
+            setName: set.setName,
+            tags: set.tags,
+            isPublic: set.isPublic,
+            views: set.views,
+            plays: set.plays,
+            studies: set.studies,
+            favorites: set.favorites
+        }
+        )
+
+    )
+
+    console.log(rows);
 
     return (
         <>
-            <Container fixed maxWidth='md' id='register-component'>
+            <Container fixed maxWidth='md' id='register-component' sx={{height:'400px'}}>
                 {userFavorites.length ?
-                    <Typography>
-                        {JSON.stringify(userFavorites)}<br/>
-                    </Typography>
+                    <DataGrid
+                        columns={columns}
+                        rows={rows}
+                        pagination={true}
+                        pageSize={5}
+                        rowsPerPageOptions={[5]}
+                    />
                     :
                     <Typography>
                         You haven't added any sets to your favorites yet!<br/>
