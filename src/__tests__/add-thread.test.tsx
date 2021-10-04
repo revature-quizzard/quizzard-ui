eb7fb16bedfe6391265ca3199719b23af918370b
metteec2 <chmettee@gmail.com>
2021-10-04 17:40:01 -0400

    Beginning testing for AddThread component
    Signed-off-by: metteec2 <chmettee@gmail.com>

---
+++ src/__tests__/add-thread.test.tsx
@@ -0,0 +1,37 @@
+import { shallow, configure, mount } from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import { Subforum } from '../models/subforum';
+jest.mock('../remote/thread-service');
+
+
+
+
+
+
+interface TempState {
+    currentSubforum: Subforum | undefined;
+}
+
+let initialState: any;
+
+describe('Add Thread Component Test Suite', () => {
+
+    beforeAll(() => {
+        configure({adapter: new Adapter()});
+    });
+
+    beforeEach(() => {
+        initialState = {
+            currentSubforum: undefined
+        }
+    });
+
+    afterEach(() => {
+        jest.resetAllMocks();
+        initialState = null;
+    });
+
+    
+
+
+});
