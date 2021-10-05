aa2bfc3c58272fb29f776c8d5e73feaca20b1dd3
LunaHaines <nhaines95@gmail.com>
2021-10-05 14:04:08 -0600

    fixed store by spying on selector

--- src/__tests__/add-comment.test.tsx
+++ src/__tests__/add-comment.test.tsx
@@ -1,13 +1,16 @@
 import { shallow, configure, mount } from 'enzyme';
+import * as redux from 'react-redux';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import AddComment from '../components/Forum/AddComment';
 import { Subforum } from '../models/subforum';
 import { Thread } from '../models/thread';import { Provider } from 'react-redux';
 import createMockStore from 'redux-mock-store';
+import forumReducer, { forumState } from '../state-slices/forum/forum-slice';
 import { addComment } from '../remote/comment-service';
-import { assert } from 'console';
 jest.mock('../remote/comment-service');
 import { showSnackbar, setErrorSeverity } from '../state-slices/error/errorSlice';
+import { act } from '@testing-library/react';
+import { configureStore } from '@reduxjs/toolkit';
 jest.mock('../state-slices/error/errorSlice')
 
 interface TempState {
@@ -47,6 +50,40 @@ describe('Add Comment Component Test Suite', () => {
 
         // expect it to be truthy (i.e. something renders)
         expect(wrapper).toBeTruthy();
+    })
+
+    it('Component calls addComment when create button is clicked', () => {
+        // set up initial state
+        initialState = {
+            currentSubforum: new Subforum([], 'NULL', 'description', 'subforumId', 'subject', 1, '2021-09-28T12:34:56.789'),
+            currentThread: new Thread(['subforumId'], 'subforumId', 'description', 'subject', 'username', 'threadId', 1, '2021-09-28T12:42:56.789')
+        }
+
+        // configure mock store
+        //@ts-ignore
+        const mockStore = configureStore({reducer: {forumInfo: forumReducer}, initialState});
+        console.log(mockStore)
+
+        const spy = jest.spyOn(redux, 'useSelector');
+        spy.mockImplementation((arg) => {
+            if (arg === forumState) {
+                return initialState;
+            } else {
+                return {
+                    authUser: { username: 'username' }
+                }
+            }
+        })
+        // set up wrapper class
+        const wrapper = mount(<Provider store={mockStore}>
+                                    <AddComment />
+                                </Provider>);
+        let createWrapper = wrapper.find('#createCommentButton').at(0);
+
+        act(() => {createWrapper.simulate('click')});
+
+        // expect addComment to have been called
+        expect(addComment).toBeCalled();
     })
 
     /*
