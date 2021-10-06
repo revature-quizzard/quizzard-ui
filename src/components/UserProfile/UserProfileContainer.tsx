f46b87958571b29764da7a88fdf9d460093ba3b3
batainem96 <batainem96@gmail.com>
2021-10-06 10:42:59 -0500

    Fixed profile re-render issue. Profile page now correctly re-renders after update/create sets.

--- src/components/UserProfile/UserProfileContainer.tsx
+++ src/components/UserProfile/UserProfileContainer.tsx
@@ -29,6 +29,8 @@ const UserProfileContainer = (props: any) => {
   const [updateSetId, setUpdateSetId] = useState(undefined as string);
   const [updateIsOpen, setUpdateIsOpen] = useState(false);
 
+  const [dummySwitch, setDummySwitch] = useState(false);
+
     /**
      * Main parent component for user profile. Retrieves user data and persists it to profile state.
      * Renders UserProfile, UserSets, UserFavoriteSets, and UserGameRecords upon successful retrieval.
@@ -51,7 +53,7 @@ const UserProfileContainer = (props: any) => {
     // componentDidMount lifecycle
     useEffect(() => {
         getData();
-    }, []);
+    }, [dummySwitch]);
 
     return (
         <div>
@@ -138,7 +140,7 @@ const UserProfileContainer = (props: any) => {
                 <AccordionDetails>
                     <Typography>
 
-                        {state.isLoaded ? <CreateSetModal/> : <> loading...<img className="welcomeBanner"
+                        {state.isLoaded ? <CreateSetModal dummySwitch={dummySwitch} setDummySwitch={setDummySwitch}/> : <> loading...<img className="welcomeBanner"
                                                                                 src="wizard.gif" alt="qwizard"
                                                                                 height="50px"/>  </>}
 
@@ -153,7 +155,8 @@ const UserProfileContainer = (props: any) => {
                     setUpdateIsOpen(false);
                 }}
             >
-                <UpdateSetModal setId={updateSetId} setName={updateSetName} isPublic={updateSetIsPublic} tagNames={updateSetTagNames}/>
+                <UpdateSetModal setId={updateSetId} setName={updateSetName} isPublic={updateSetIsPublic} tagNames={updateSetTagNames}
+                    dummySwitch={dummySwitch} setDummySwitch={setDummySwitch} setUpdateIsOpen={setUpdateIsOpen}/>
             </Modal>
           
         </div>
