f40dd50fa7451c2c35f54756ba5b04775725a69e
Sean Dunn <60554213+spdunn@users.noreply.github.com>
2021-10-05 21:18:15 -0400

    Misc styling changes

--- src/components/Multiplayer/Questions.tsx
+++ src/components/Multiplayer/Questions.tsx
@@ -80,8 +80,8 @@ import { guestState } from '../../state-slices/multiplayer/guest-slice';
                         { 
                         game.matchState == 2 && currentPlayer.pointsEarned > 0 ?
                             <Typography variant="h5" className={classes.pointsEarned}>
-                                + {currentPlayer.pointsEarned} points! Streak: {currentPlayer.streak} &#x1F525;
-                                <img src='wizard.gif'/>
+                                + {currentPlayer.pointsEarned} points! {currentPlayer.streak > 1 ? <>Streak: {currentPlayer.streak} &#x1F525; </>: <> </>}
+                                <img height='100px' src='wizard.gif'/>
                             </Typography> : game.matchState == 2 ?
                             <Typography variant="h5" className={classes.noPointsEarned}>
                                 No points earned... &#x1F4A9;
